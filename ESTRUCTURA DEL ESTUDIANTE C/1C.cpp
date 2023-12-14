d51eb9e513108a355febba7107311ae3273405d3
Sabil667 <msabi@myuax.com>
2023-12-14 12:47:53 +0100

    .

--- ESTRUCTURA DEL ESTUDIANTE C/1C.cpp
+++ ESTRUCTURA DEL ESTUDIANTE C/1C.cpp
@@ -5,3 +5,10 @@ struct Estudiante {
     int edad;
     float promedio;
 };
+
+// Función para mostrar todos los campos del estudiante
+void mostrarEstudiante(const struct Estudiante* estudiante) {
+    printf("Nombre: %s\n", estudiante->nombre);
+    printf("Edad: %d \n", estudiante->edad);
+    printf("Promedio: %.2f\n", estudiante->promedio);
+}
