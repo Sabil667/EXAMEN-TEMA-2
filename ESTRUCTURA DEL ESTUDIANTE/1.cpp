8aa58d2a65b1601ca21311fc5c4799514753a3a1
Sabil667 <msabi@myuax.com>
2023-12-14 12:39:10 +0100

    .

--- ESTRUCTURA DEL ESTUDIANTE/1.cpp
+++ ESTRUCTURA DEL ESTUDIANTE/1.cpp
@@ -5,3 +5,9 @@ struct Estudiante {
     int edad;
     double promedio;
 };
+// Función para mostrar todos los campos del estudiante
+void mostrarEstudiante(const Estudiante& estudiante) {
+    std::cout << "Nombre: " << estudiante.nombre << std::endl;
+    std::cout << "Edad: " << estudiante.edad << std::endl;
+    std::cout << "Promedio: " << estudiante.promedio << std::endl;
+}
